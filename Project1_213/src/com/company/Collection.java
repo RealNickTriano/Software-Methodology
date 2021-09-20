f362cca0082ebbc98c30cb1bf6add3c818e270a3
tonsta <antonio.ignarra1999@gmail.com>
2021-09-20 10:22:46 -0400

    added remove and cleaned up some stuff throughout

--- Project1_213/src/com/company/Collection.java
+++ Project1_213/src/com/company/Collection.java
@@ -5,33 +5,37 @@
 package com.company;
 
 public class Collection {
+
+    // instance variables
+    private Album[] albums;
+    private int numAlbums; //number of albums currently in the collection
+
+    // constructor
     public Collection(Album[] albums, int numAlbums) {
         this.albums = albums;
         this.numAlbums = numAlbums;
     }
 
-    private Album[] albums;
-    private int numAlbums; //number of albums currently in the collection
-
     public int getNumAlbums()
     {
         return numAlbums;
     }
+
     private int find(Album album) //find the album index, or return NOT_FOUND
     {
         int i;
         for(i=0; i<numAlbums; i++){
-            if (albums[i].getTitle() == album.getTitle() && albums[i].getArtist() == album.getArtist()){
+            if (albums[i].equals(album)){
                 return i;
             }
         }
         return Constants.NOT_FOUND;
     }
 
-    private void grow() //increase the capacity of the array list by 4
+    private void grow() // increase the capacity of the array list by 4
     {
         Album[] newAlbumArr = new Album[numAlbums + 4];
-        //copy over albums in the current collection to the collection w/ updated size
+        // copy over albums in the current collection to the collection w/ updated size
         for(int i=0; i<numAlbums; i++ ){
             newAlbumArr[i] = albums[i];
         }
@@ -40,21 +44,42 @@ public class Collection {
 
     public boolean add(Album album)
     {
-        if(numAlbums == 0)
-        {
+        if (numAlbums == 0) {
             this.grow();
         }
-        if (numAlbums >= albums.length){
+        else if (numAlbums >= albums.length) {
             this.grow();
         }
         albums[numAlbums] = album;
         numAlbums++;
         return true;
     }
+
     public boolean remove(Album album)
     {
+        // find the index of the album to be removed
+        int i = find(album);
+        // check if requested album is in the collection
+        if (i == Constants.NOT_FOUND){
+            return false;
+        }
+        // remove album
+        else{
+            for(; i<numAlbums; i++){
+                // if collection is at full capacity
+                if (i == albums.length - 1){
+                    Date date = new Date("12/25/2030");
+                    albums[i] = new Album("none", "none", Genre.unknown, date, true);
+                }
+                else{
+                    albums[i] = albums[i+1];
+                }
+            }
+        }
+        numAlbums--;
         return true;
     }
+
     public boolean lendingOut(Album album) //set to not available
     {
         // if already not available return false for error
@@ -65,6 +90,7 @@ public class Collection {
         album.setIsAvailable(false);
         return true;
     }
+
     public boolean returnAlbum(Album album) //set to available
     {
         // if already available return false for error
@@ -75,13 +101,14 @@ public class Collection {
         album.setIsAvailable(true);
         return true;
     }
+
     public void print() //display the list without specifying the order
     {
-        for(int i = 0; i < numAlbums; i++)
-        {
+        for(int i = 0; i < numAlbums; i++) {
             System.out.println(albums[i]);
         }
     }
+
     public void printByReleaseDate()
     {
         Album[] newAlbums = new Album[numAlbums];
@@ -101,6 +128,7 @@ public class Collection {
             System.out.println(newAlbums[i]);
         }
     }
+
     public void printByGenre()
     {
        /** Album[] newAlbums = new Album[numAlbums];
