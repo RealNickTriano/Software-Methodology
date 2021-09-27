8f2e64dac272a00c4aee74ff9f14aab57919e829
tonsta <antonio.ignarra1999@gmail.com>
2021-09-27 09:41:32 -0400

    some test cases for date class

--- Project1_213/src/com/company/Date.java
+++ Project1_213/src/com/company/Date.java
@@ -154,4 +154,76 @@ public class Date implements Comparable<Date> {
             return 0;
 
     }
+
+    /** Testbed main for the Date class */
+    public static void main(String[] args){
+        //test case #1, a date with a year less than 1980 should be invalid.
+        Date date = new Date("11/1/1979");
+        boolean expectedResult = false;
+        boolean result = date.isValid();
+        System.out.print("Test case #1: ");
+        if (result == expectedResult){
+            System.out.println("Pass.");
+        }
+        else{
+            System.out.println("Fail.");
+        }
+
+        //test case #2, a date beyond today's date should be invalid.
+        date = new Date("11/1/2022");
+        result = date.isValid();
+        System.out.print("Test case #1: ");
+        if (result == expectedResult){
+            System.out.println("Pass.");
+        }
+        else{
+            System.out.println("Fail.");
+        }
+
+        //test case #3, a date with an invalid month.
+        date = new Date("13/1/2020");
+        result = date.isValid();
+        System.out.print("Test case #1: ");
+        if (result == expectedResult){
+            System.out.println("Pass.");
+        }
+        else{
+            System.out.println("Fail.");
+        }
+
+        //test case #4, an invalid leap year day.
+        date = new Date("2/29/2021");
+        result = date.isValid();
+        System.out.print("Test case #1: ");
+        if (result == expectedResult){
+            System.out.println("Pass.");
+        }
+        else{
+            System.out.println("Fail.");
+        }
+
+        //test case #5, another invalid leap year day.
+        date = new Date("2/29/2018");
+        result = date.isValid();
+        System.out.print("Test case #1: ");
+        if (result == expectedResult){
+            System.out.println("Pass.");
+        }
+        else{
+            System.out.println("Fail.");
+        }
+
+        //test case #6, an invalid day (April has 30 days).
+        date = new Date("4/31/2021");
+        result = date.isValid();
+        System.out.print("Test case #1: ");
+        if (result == expectedResult){
+            System.out.println("Pass.");
+        }
+        else{
+            System.out.println("Fail.");
+        }
+
+    }
+
 }
