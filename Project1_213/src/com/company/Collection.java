2da987b247abd37eb6e19dbfa3219fab25b49d9a
tonsta <antonio.ignarra1999@gmail.com>
2021-09-27 17:29:44 -0400

    spacing in collection

--- Project1_213/src/com/company/Collection.java
+++ Project1_213/src/com/company/Collection.java
@@ -65,11 +65,13 @@ public class Collection {
      * @return false if the collection already contains the given album, true if the album was successfully added
      */
     public boolean add(Album album) {
-        if (find(album) != Constants.NOT_FOUND)
+        if (find(album) != Constants.NOT_FOUND){
             return false;
+        }
         if (numAlbums == 0) {
             this.grow();
-        } else if (numAlbums >= albums.length) {
+        }
+        else if (numAlbums >= albums.length) {
             this.grow();
         }
         albums[numAlbums] = album;
