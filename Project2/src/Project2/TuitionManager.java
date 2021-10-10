f23dc416ef4c10d94f22d3925483a82ea5292160
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2021-10-09 21:07:07 -0400

    Exceptions Exceptions Exceptions

--- Project2/src/Project2/TuitionManager.java
+++ Project2/src/Project2/TuitionManager.java
@@ -52,13 +52,56 @@ public class TuitionManager {
      * Separate input string into different strings
      * @param st1 The whole line of input from user
      */
-    private void tokenizeAdd(StringTokenizer st1) {
+    private int tokenizeAdd(StringTokenizer st1) {
+
+        // check token count
+        int count = st1.countTokens();
+        if( count < 2) {
+            System.out.println("Missing data in command line.");
+        }
+        else if ( count < 3 )
+        {
+            System.out.println( "Credit hours missing." );
+        }
+            studentName = st1.nextToken();
+            majorString = st1.nextToken();
+            majorString.toUpperCase();
+            newMajor = Major.valueOf(majorString);
+            creditHoursString = st1.nextToken();
+            try {
+                credits = Integer.parseInt(creditHoursString);
+            }
+            catch ( Exception e )
+            {
+                System.out.println( "Invalid credit hours." );
+                return -1;
+            }
+            if ( credits < 3 ) {
+                System.out.println( "Minimum credit hours is 3." );
+                throw new ArithmeticException( "Minimum credit hours is 3." );
+            }
+            else if ( credits > 24 ){
+                System.out.println( "Credit hours exceed the maximum 24." );
+                throw new ArithmeticException( "Credit hours exceed the maximum 24." );
+            }
+            else if ( credits < 0 ){
+                System.out.println( "Credit hours cannot be negative." );
+                throw new ArithmeticException( "Credit hours cannot be negative." );
+            }
+
+            if (command.equalsIgnoreCase("AT")) // if tri state
+                state = st1.nextToken();
+            else if (command.equalsIgnoreCase("AI")) // if international
+                studyAbroad = Boolean.parseBoolean(st1.nextToken());
+
+        return 1;
+    }
+
+    private void tokenizeRemove(StringTokenizer st1) {
 
         studentName = st1.nextToken();
         majorString = st1.nextToken();
         newMajor = Major.valueOf(majorString);
-        creditHoursString = st1.nextToken();
-        credits = Integer.parseInt(creditHoursString);
     }
 
     /**
@@ -68,44 +111,60 @@ public class TuitionManager {
      */
     private void handleCommand(String command, StringTokenizer st1) {
         boolean error;
-        newProfile  = new Profile(studentName, newMajor);
         switch (command) {
             default:
                 printCommand(command);
                 break;
             case "AR":
                 // add a resident student
-                tokenizeAdd(st1);
-                Resident newResident = new Resident(newProfile, credits, 0, 0, new Date());
-                error = roster.add(newResident);
-                handleErrorAdd(error, newResident);
+                try {
+                    int err = tokenizeAdd(st1);
+                    if ( err == -1 )
+                        break;
+                    newProfile  = new Profile(studentName, newMajor);
+                    Resident newResident = new Resident(newProfile, credits, 0, 0, new Date());
+                    error = roster.add(newResident);
+                    handleErrorAdd(error);
+                }
+                catch ( Exception e )
+                {
+
+                }
+
                 break;
             case "AN":
                 // add a nonresident student
                 tokenizeAdd(st1);
+                newProfile  = new Profile(studentName, newMajor);
                 NonResident newNonResident = new NonResident(newProfile, credits, 0, 0, new Date());
                 error = roster.add(newNonResident);
-                handleErrorAdd(error, newNonResident);
+                handleErrorAdd(error);
                 break;
             case "AT":
                 // add a tristate student
                 tokenizeAdd(st1);
+                newProfile  = new Profile(studentName, newMajor);
                 TriState newTriState = new TriState(newProfile, credits, state, 0, 0, new Date());
                 error = roster.add(newTriState);
-                handleErrorAdd(error, newTriState);
+                handleErrorAdd(error);
                 break;
             case "AI":
                 // add a international student
                 tokenizeAdd(st1);
+                newProfile  = new Profile(studentName, newMajor);
                 International newInternational = new International(newProfile, credits, studyAbroad, 0, 0, new Date());
                 error = roster.add(newInternational);
-                handleErrorAdd(error, newInternational);
+                handleErrorAdd(error);
                 break;
             case "R":
                 //Remove a student
                 // TODO: need to make this student first
-                error = roster.remove(newStudent);
-                handleErrorRemove(error, newStudent);
+                tokenizeRemove(st1);
+                newProfile  = new Profile(studentName, newMajor);
+                // make student to compare profiles
+                Student student = new Student(newProfile, 0, 0, new Date() );
+                error = roster.remove(student);
+                handleErrorRemove(error);
                 break;
             case "C":
                 // Calculate tuition dues
@@ -156,19 +215,19 @@ public class TuitionManager {
         }
     }
 
-    private void handleErrorAdd(boolean error, Student newStudent)
+    private void handleErrorAdd(boolean error)
     {
         if (!error)
-            System.out.println(newStudent + " >> is already on the roster.");
+            System.out.println( "Student is already in the roster." );
         else
-            System.out.println(newStudent + " >> added successfully.");
+            System.out.println( "Student added." );
     }
 
-    private void handleErrorRemove(boolean error, Student newStudent)
+    private void handleErrorRemove(boolean error)
     {
         if (!error)
-            System.out.println(newStudent + " >> is not on the roster.");
+            System.out.println( "Student is not in the roster." );
         else
-            System.out.println(newStudent + " >> removed successfully.");
+            System.out.println( "Student removed from the roster." );
     }
 }
