bd3dc5a1da163b97f68165ae6ae6c9bbda999483
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2021-10-11 17:54:12 -0400

    date test is good

--- Project2/src/Project2/DateTest.java
+++ Project2/src/Project2/DateTest.java
@@ -1,13 +1,49 @@
 package Project2;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Nicholas Triano, Antonio Ignarra
  */
 class DateTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void isValid() {
+
+        Date date;
+
+        //test case #1, a date with a year less than 2021 should be invalid.
+        date = new Date( "6/1/2020" );
+        Assertions.assertFalse(date.isValid());
+
+        //test case #2, a date beyond today's date should be invalid.
+        date = new Date();
+        date.setDay( date.getDay() + 1 );
+        Assertions.assertFalse(date.isValid());
+
+        //test case #3, a date with an invalid month.
+        date = new Date( "13/1/2021" );
+        Assertions.assertFalse(date.isValid());
+
+        //test case #4, an invalid month.
+        date = new Date( "0/22/2021" );
+        Assertions.assertFalse(date.isValid());
+
+        //test case #5, an invalid day (April has 30 days).
+        date = new Date( "4/31/2021" );
+        Assertions.assertFalse(date.isValid());
+
+        //test case #6, an invalid day (January has 31 days).
+        date = new Date( "1/32/2021" );
+        Assertions.assertFalse(date.isValid());
+
+        // test case #7, a valid date
+        date = new Date( "8/5/2021" );
+        Assertions.assertTrue(date.isValid());
+
+        // test case #8, a valid date
+        date = new Date( "2/2/2021" );
+        Assertions.assertTrue(date.isValid());
     }
 }
