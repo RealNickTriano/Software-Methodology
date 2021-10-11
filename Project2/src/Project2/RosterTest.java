fd437f6176792d50e3d5e188b580d5d189df8854
tonsta <antonio.ignarra1999@gmail.com>
2021-10-11 17:55:21 -0400

    rosterTest

--- Project2/src/Project2/RosterTest.java
+++ Project2/src/Project2/RosterTest.java
@@ -1,10 +1,25 @@
 package Project2;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Nicholas Triano, Antonio Ignarra
  */
 class RosterTest {
 
+    @Test
+    public void testAdd() {
+        Student[] studentArray;
+        Roster myRoster = new Roster(studentArray, 0);
+        Student myStudent = new Student();
+        myRoster.add(myStudent);
+        Assertions.assertfalse(myRoster.add(myStudent));
+
+    }
+
+    @Test
+    public void testRemove() {
+
+    }
 }
