e54e44f613916fd0ac35b5f62b5a11a75a744e9a
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2021-10-28 10:41:35 -0400

    status radio buttons work, states are greyed out when tristate is unselected

--- Project3/src/main/java/com/example/project3/HelloController.java
+++ Project3/src/main/java/com/example/project3/HelloController.java
@@ -1,15 +1,50 @@
 package com.example.project3;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
 
 public class HelloController {
     @FXML
     private Label systemText;
-    private Label systemDialog;
+    @FXML
+    private DialogPane systemDialog;
+    @FXML
+    private TextField name;
+    @FXML
+    private RadioButton tristateButton;
+    @FXML
+    private RadioButton newYork;
+    @FXML
+    private RadioButton connecticut;
+    @FXML
+    private RadioButton international;
 
     @FXML
     protected void onHelloButtonClick() {
         systemText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    protected  void onTristateButtonClick()
+    {
+            newYork.setOpacity(1.0);
+            newYork.setDisable(false);
+            connecticut.setOpacity(1.0);
+            connecticut.setDisable(false);
+    }
+
+    @FXML
+    protected  void onInternationalButtonClick()
+    {
+            newYork.setOpacity(0.5);
+            newYork.setDisable(true);
+            newYork.setSelected(false);
+            connecticut.setOpacity(0.5);
+            connecticut.setDisable(true);
+            connecticut.setSelected(false);
+
+    }
+
+
 }
