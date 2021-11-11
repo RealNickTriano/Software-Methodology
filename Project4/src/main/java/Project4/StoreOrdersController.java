46cfe050170f872ac42ac7662c3fe641eb303692
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2021-11-11 11:37:06 -0500

    storeOrdersView Ui added

--- Project4/src/main/java/Project4/StoreOrdersController.java
+++ Project4/src/main/java/Project4/StoreOrdersController.java
@@ -1,14 +1,31 @@
 package Project4;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
 
 public class StoreOrdersController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView storeOrdersListView;
+    @FXML
+    private Button exportOrdersButton;
+    @FXML
+    private Button cancelOrderButton;
+    @FXML
+    private ComboBox phoneCombo;
+    @FXML
+    private TextField orderTotalTextField;
+
+    @FXML
+    protected void handleCancelOrder()
+    {
+
+    }
+
+    @FXML
+    protected void handleExportOrders()
+    {
+
     }
 }
