38a9da5b74402f6378823f9cd1f04e2c6b9f680c
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2021-11-11 11:13:27 -0500

    set up ui for currentOrderView

--- Project4/src/main/java/Project4/CurrentOrderController.java
+++ Project4/src/main/java/Project4/CurrentOrderController.java
@@ -1,14 +1,36 @@
 package Project4;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class CurrentOrderController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView orderListView;
+    @FXML
+    private Button removePizzaButton;
+    @FXML
+    private Button placeOrderButton;
+    @FXML
+    private TextField subtotalTextField;
+    @FXML
+    private TextField salesTaxTextField;
+    @FXML
+    private TextField orderTotalTextField;
+
+    @FXML
+    protected void handleRemovePizzaButton()
+    {
+
+    }
+
+    @FXML
+    protected void handlePlaceOrderButton()
+    {
+
     }
 }
