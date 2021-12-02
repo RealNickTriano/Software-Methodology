c7612b54a69a436f7de7afe210623ceb6f88cd9c
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2021-12-02 10:34:47 -0500

    added activities for each controller there was in p4

---
+++ Project5/app/src/main/java/com/example/project5/StoreOrdersActivity.java
@@ -0,0 +1,14 @@
+package com.example.project5;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoreOrdersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_orders);
+    }
+}
