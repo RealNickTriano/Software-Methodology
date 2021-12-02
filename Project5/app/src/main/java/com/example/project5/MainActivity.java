2e24cc4da4af16fd7f1b3b24ae0570cfaf956336
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2021-12-02 11:59:59 -0500

    added variables to main

--- Project5/app/src/main/java/com/example/project5/MainActivity.java
+++ Project5/app/src/main/java/com/example/project5/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.project5;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button pepperoniButton;
+    private Button deluxeButton;
+    private Button hawaiianButton;
+    private Button storeOrdersButton;
+    private Button currentOrderButton;
+    private ContactsContract.CommonDataKinds.Phone phoneNumber;
+    protected Order order;
+    protected StoreOrders storeOrder;
+    protected ArrayList<Order> storeOrderList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
