4b85dc6c243861737131fb9c1bae1e4dcce81281
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2021-12-06 12:13:11 -0500

    order shows in store orders

--- Project5/app/src/main/java/com/example/project5/StoreOrdersActivity.java
+++ Project5/app/src/main/java/com/example/project5/StoreOrdersActivity.java
@@ -1,21 +1,30 @@
 package com.example.project5;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class StoreOrdersActivity extends AppCompatActivity {
+public class StoreOrdersActivity extends AppCompatActivity implements RecyclerAdapter.OnPizzaListener {
 
     private ArrayList<Order> storeOrders;
     private Spinner phoneList;
     private TextView subtotal;
     private TextView tax;
     private TextView total;
+    private Button cancelOrderButton;
+    private RecyclerView recyclerView;
+    private TextView removeText;
+    private ArrayList<Pizza> order = new ArrayList<Pizza>();
 
 
     @Override
@@ -27,6 +36,7 @@ public class StoreOrdersActivity extends AppCompatActivity {
         total = findViewById(R.id.totalPrice);
         phoneList= (Spinner) findViewById(R.id.phoneSpinner);
         storeOrders = MainActivity.storeOrder.getStoreOrdersList();
+        recyclerView = findViewById(R.id.recyclerViewStore);
         int size = storeOrders.size();
 
         String[] arraySpinner = new String[size];
@@ -38,6 +48,7 @@ public class StoreOrdersActivity extends AppCompatActivity {
                 android.R.layout.simple_spinner_item, arraySpinner);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         phoneList.setAdapter(adapter);
+        setRecyclerAdapter();
         setPrices();
     }
 
@@ -49,5 +60,37 @@ public class StoreOrdersActivity extends AppCompatActivity {
                 total.setText(String.format("$%.2f", storeOrders.get(i).getTotal() + (MainActivity.order.getTotal() * Constants.TAX_RATE)));
             }
         }
+    }
+
+    public void setOrder()
+    {
+        for(int i=0; i <storeOrders.size(); i++) {
+            if (phoneList.getSelectedItem().toString() == storeOrders.get(i).getPhoneNumber()) {
+                order = storeOrders.get(i).getPizzaList();
+            }
+        }
+    }
+
+    private void setRecyclerAdapter() {
+        setOrder();
+        RecyclerAdapter adapter = new RecyclerAdapter(order, this);
+        RecyclerView.LayoutManager layoutManager =
+                new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        removeText = recyclerView.findViewById(R.id.removeText);
+        //removeText.setText(" ");
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onPizzaClick(int position) {
+
+    }
+
+    public void handleCancelOrder(View view)
+    {
+
     }
 }
